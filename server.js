80809e6ec47217eb8b8ba74a6b44fd75228311f7
ceci-00 <ceci.ga003@gmail.com>
2024-03-26 21:23:45 -0500

    created route to get index.html

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000
 const dbData = require('./Develop/db/db.json');
+
+//html routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+})
+// get * should return index.html
+app.get()
